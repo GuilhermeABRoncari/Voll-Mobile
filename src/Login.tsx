2d1011baa80e91cb676d6d4982f0de54e7f4c9c0
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2024-03-06 00:02:02 -0300

    chama os componeentes em Login

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,41 +1,21 @@
-import { Box, Button, FormControl, Image, Input, Link, Text, VStack } from 'native-base';
+import { Box, Image, Link, Text, VStack } from 'native-base';
 import { TouchableOpacity } from 'react-native';
+
 import Logo from './assets/Logo.png';
+import { Botao } from './componentes/Botao';
+import { EntradaTexto } from './componentes/EntradaTexto';
+import { Titulo } from './componentes/Titulo';
 
 export default function Login() {
   return (
     <VStack flex={1} alignItems={'center'} justifyContent={'center'} padding={5}>
       <Image source={Logo} alt="Logo Voll" />
-      <Text fontSize={'2xl'} fontWeight={'bold'} color={'gray.500'} textAlign={'center'} marginTop={5}>
-        Faça login em sua conta
-      </Text>
+      <Titulo>Faça login na sua conta</Titulo>
       <Box>
-        <FormControl marginTop={3}>
-          <FormControl.Label>Email</FormControl.Label>
-          <Input
-            placeholder="Insira seu endereço de email"
-            size={'lg'}
-            width={'100%'}
-            borderRadius={'lg'}
-            backgroundColor={'gray.100'}
-            shadow={3}
-          />
-        </FormControl>
-        <FormControl marginTop={3}>
-          <FormControl.Label>Senha</FormControl.Label>
-          <Input
-            placeholder="Insira sua senha"
-            size={'lg'}
-            width={'100%'}
-            borderRadius={'lg'}
-            backgroundColor={'gray.100'}
-            shadow={3}
-          />
-        </FormControl>
+        <EntradaTexto label={'Email'} placeholder={'Insira seu endereço de email'} />
+        <EntradaTexto label={'Senha'} placeholder={'Insira sua senha'} />
       </Box>
-      <Button width={'100%'} backgroundColor={'blue.800'} marginTop={10} borderRadius={'lg'}>
-        Entrar
-      </Button>
+      <Botao>Entrar</Botao>
       <Link href="" marginTop={5}>
         Esqueceu sua senha?
       </Link>
