3f68340aad6e95849c98e4cfdaa932e15cab4b3c
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2024-03-05 22:48:22 -0300

    Criando tela principal

---
+++ src/principal.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+interface Props {
+  texto: string;
+}
+
+export default function Principal() {
+  return (
+    <View>
+      <Text>Tela Principal</Text>
+    </View>
+  );
+}
