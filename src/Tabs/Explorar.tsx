3553b573c6fb4a457d465c72151b1d0bf0fd43fc
GuilhermeABRoncari <guilhermeabroncari@hotmail.com>
2024-03-11 20:24:11 -0300

    Cria tela 'Explorar'

--- src/Tabs/Explorar.tsx
+++ src/Tabs/Explorar.tsx
@@ -1,9 +1,33 @@
-import { Text, VStack } from 'native-base';
+import { Box, ScrollView, VStack } from 'native-base';
+import { Botao } from '../componentes/Botao';
+import CardConsulta from '../componentes/CardConsulta';
+import { EntradaTexto } from '../componentes/EntradaTexto';
+import { Titulo } from '../componentes/Titulo';
+import { consultas } from '../utils/Consultas';
 
 export default function Explorar() {
   return (
-    <VStack>
-      <Text>Explorar</Text>
-    </VStack>
+    <ScrollView padding={5} backgroundColor={'white'}>
+      <VStack padding={5} shadow={'1'} backgroundColor={'white'} marginTop={5}>
+        <Box>
+          <EntradaTexto label={''} placeholder={'Digite a especialidade'} />
+          <EntradaTexto label={''} placeholder={'Digite sua localização'} />
+        </Box>
+        <Botao>Buscar</Botao>
+      </VStack>
+      <Titulo color={'blue.500'} alignSelf={'center'} marginTop={5}>
+        Resultado da busca
+      </Titulo>
+      <VStack padding={5} shadow={'1'} backgroundColor={'white'} marginTop={5}>
+        {consultas?.map((consulta) => (
+          <CardConsulta
+            key={consulta.id}
+            photo={consulta.photo}
+            medico={consulta.medico}
+            especialidade={consulta.especialidade}
+          />
+        ))}
+      </VStack>
+    </ScrollView>
   );
 }
